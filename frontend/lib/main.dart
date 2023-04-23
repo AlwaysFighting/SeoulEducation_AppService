03759e4862856c0d9c7fdec498928d301794a10b
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-04-23 20:44:53 +0900

    Feat: 회원가입 UI 90% 완성

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:seoul_education_service/logins/login/views/login_page.dart';
+import 'const/navigation.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Spoqa Han Sans Neo'),
-      home: const LoginPage(),
+      home: const Navigation(),
     ),
   );
 }
