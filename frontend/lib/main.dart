bec9996da438593fdf799d11d5933c485c398968
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-04-30 02:57:51 +0900

    feat: 카카오톡 로그인 완료

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:seoul_education_service/logins/login/views/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+
+  print(FlutterConfig.get('KAKAOLOGIN_APP_KEY'));
+
+  KakaoSdk.init(
+    nativeAppKey: '${FlutterConfig.get('KAKAOLOGIN_APP_KEY')}',
+  );
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
