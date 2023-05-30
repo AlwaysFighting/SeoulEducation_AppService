2f8b4ae5db17809e00d4bbea69a0b785076a6928
nusuy <nsuy.ch@gmail.com>
2023-05-30 14:26:38 +0900

    chore: Modify db name
    - into ``.env``

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,7 @@ dotenv.config();
 const development = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "database_seoul01",
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: "mysql",
   dialectOptions: { decimalNumbers: true },
